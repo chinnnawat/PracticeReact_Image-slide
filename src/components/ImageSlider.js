f7c86df7855b659cb903e1bc8c2e7e849e440e5c
chinnnawat <chin3896@gmail.com>
2023-09-25 16:17:13 +0700

    completed

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,18 +1,41 @@
 import ImageData from "./ImageData"
 import '../App.css';
 import { BsFillArrowLeftCircleFill,BsFillArrowRightCircleFill } from "react-icons/bs";
+import { useState } from "react";
 const ImageSlider = () =>{
+    const[current,setCurrent] = useState(0)
+    const prevSlide=()=>{
+        // setCurrent(current-1)
+        if(current<=0){
+            setCurrent(ImageData.length-1)
+        }
+        else{
+            setCurrent(current-1)
+        }
+    }
+    const nextSlide=()=>{
+        if(current>=ImageData.length-1){
+            setCurrent(0)
+        }
+        else{
+            setCurrent(current+1)
+        }
+    }
+
     return(
         <section className="slider">
-            <BsFillArrowLeftCircleFill className="leftArrow"/>
-            <BsFillArrowRightCircleFill className="rightArrow"/>
+            <BsFillArrowLeftCircleFill className="leftArrow"onClick={prevSlide}/>
+            <BsFillArrowRightCircleFill className="rightArrow"onClick={nextSlide}/>
             {ImageData.map((data,index)=>{
                 return(
-                    <div className="slide" key={index}>
-                        <div>
-                            <img src={data.image} alt={data.title} className="image"></img>
-                            <p>{data.title}</p>
-                        </div>
+                    <div className={index === current ? "slide active": "slide"} key={index}>
+                        {index === current &&
+                        (
+                            <div>
+                                <img src={data.image} alt={data.title} className="image"></img>
+                                <p>{data.title}</p>
+                            </div>
+                        )}
                     </div>
                 )
             })}
